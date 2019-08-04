cbaa0532ef9d9164b143ecfeab429fdaf9d21469
hrn0 <51802564+hrn0@users.noreply.github.com>
2019-08-04 22:29:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ var wmg2 = (wmg-1000)/2
 text.style.margin = `200px ${wmg2}px 50px`;
 
 if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)){
+  text.style.backgroundColor = 'pink';
   document.getElementById('css').href = 'style2.css';
 }
