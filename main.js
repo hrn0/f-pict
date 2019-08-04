fea9fa2bfa88aa28a9acb6d807c9beb2797ffb87
hrn0 <51802564+hrn0@users.noreply.github.com>
2019-08-04 22:23:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,5 @@ var wmg2 = (wmg-1000)/2
 text.style.margin = `200px ${wmg2}px 50px`;
 
 if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)){
-}else {
-
   document.getElementById('css').href = 'style2.css';
 }
