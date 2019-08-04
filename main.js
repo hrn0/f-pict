55a4c0e108de5efd88f4a3568d157f8a3d329ee6
hrn0 <51802564+hrn0@users.noreply.github.com>
2019-08-04 23:27:19 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,17 @@
 // text.style.margin = `200px ${wmg2}px 50px`;
 text.style.margin = `200px auto 50px`;
 
+var num = 0;
+if(num == 0){
 if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)){
   text.style.backgroundColor = 'pink';
   //document.getElementById('css').href = 'style2.css';
   window.location.href = 'index2.html';
+  num = 1;
 }else{
   text.style.backgroundColor = 'blue';
   //document.getElementById('css').href = 'style.css';
   window.location.href = 'index.html';
+  num = 1;
 }
+};
