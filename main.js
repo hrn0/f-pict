9126cff48d5652ce56365ce6621800386c302b42
hrn0 <51802564+hrn0@users.noreply.github.com>
2019-08-04 23:14:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-var wmg = window.innerWidth;
-var wmg2 = (wmg-1000)/2
-text.style.margin = `200px ${wmg2}px 50px`;
+// var wmg = window.innerWidth;
+// var wmg2 = (wmg-1000)/2
+// text.style.margin = `200px ${wmg2}px 50px`;
+text.style.margin = `200px auto 50px`;
 
 if(navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/i)){
   text.style.backgroundColor = 'pink';
-  document.getElementById('css').href = 'style2.css';
+  //document.getElementById('css').href = 'style2.css';
+  window.location.href = 'index2.html';
 }else{
   text.style.backgroundColor = 'blue';
-  document.getElementById('css').href = 'style.css';
+  //document.getElementById('css').href = 'style.css';
+  window.location.href = 'index.html';
 }
