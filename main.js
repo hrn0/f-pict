a6fb5457e0128b2844beedb8f77a376ed8d02a68
hrn0 <51802564+hrn0@users.noreply.github.com>
2019-08-05 16:56:28 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // var wmg = window.innerWidth;
 // var wmg2 = (wmg-1000)/2
 // text.style.margin = `200px ${wmg2}px 50px`;
-text.style.margin = `200px auto 50px`;
+//text.style.margin = `200px auto 50px`;
 
 var num = 0;
 if(num == 0){
